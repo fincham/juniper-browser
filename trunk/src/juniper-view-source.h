77f145ac18c75faa6c96d0e384c111af7bbeee69
fincham <fincham@767c508d-d83b-0410-8cff-fd39cd899a2f>
2008-03-24 04:27:49 +0000

    Added a status bar with row/col display for the gtksourceview to the View Source dialog.

--- trunk/src/juniper-view-source.h
+++ trunk/src/juniper-view-source.h
@@ -1,8 +1,17 @@
 #ifndef JUNIPER_VIEW_SOURCE
 #define JUNIPER_VIEW_SOURCE
 
+#include "gtk/gtk.h"
+#include "gtksourceview/gtksourceview.h"
+
 /**
  * Display a view source window for the source of the current tab.
  */
 void juniper_view_source_current_tab();
+
+/**
+ * Update the status bar when the caret is moved in the gtksourceview.
+ */
+void juniper_view_source_mark_set(GtkSourceView *source_view, GtkTextIter *location, GtkTextMark *mark, GtkStatusbar *status_bar);
+
 #endif
